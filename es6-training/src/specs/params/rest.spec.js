bb9e46002762aae2b9e4757be28794a1e13a3926
Wojciech Prokop <wojciech.w.prokop@gmail.com>
2017-03-19 10:05:39 +0100

    Rest and spread tests

--- es6-training/src/specs/params/rest.spec.js
+++ es6-training/src/specs/params/rest.spec.js
@@ -17,17 +17,17 @@ describe('rest parameters', () => {
       return others.length == arguments.length
     }
 
-    expect(resty()).toBe(/* YOUR ANSWER */)
-    expect(resty(1)).toBe(/* YOUR ANSWER */)
-    expect(resty(1, 2)).toBe(/* YOUR ANSWER */)
-    expect(resty(1, 2, 3)).toBe(/* YOUR ANSWER */)
-    expect(resty(1, 2, 3, undefined, 5, undefined, 7, undefined, 9, 10)).toBe(/* YOUR ANSWER */)
+    expect(resty()).toBe(true)
+    expect(resty(1)).toBe(false)
+    expect(resty(1, 2)).toBe(false)
+    expect(resty(1, 2, 3)).toBe(false)
+    expect(resty(1, 2, 3, undefined, 5, undefined, 7, undefined, 9, 10)).toBe(false)
   })
 
   it('it can default all arguments, optionally', () => {
     // Modify the method signature of `myFunction` to allow for all args to be optional
 
-    function myFunction({name, age, favoriteBand}) {
+    function myFunction({name="John", age=40, favoriteBand="The Beatles"} = {}) {
       expect(name).toBeDefined()
       expect(age).toBeDefined()
       expect(favoriteBand).toBeDefined()
@@ -40,11 +40,15 @@ describe('rest parameters', () => {
     myFunction()
   })
 
-  describe('gimmePairs function', function() {
+  fdescribe('gimmePairs function', function() {
     // write function `gimmePairs` which accepts dynamic number of parameters
     // and returns an array of all possible pairs
 
-    let gimmePairs;
+    let gimmePairs = function(...params) {
+      let pairs = [];
+      params.forEach((first, index) => params.slice(index + 1).forEach(second => pairs.push([first, second])));
+      return pairs;
+    };
 
     beforeEach(function() {
       jasmine.addMatchers(customMatchers);
